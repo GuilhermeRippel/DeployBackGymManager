b8d5df7978d72ebdf998b44d42abd71cc1767db1
Guilherme <guilhermerippelmoraes@gmail.com>
2025-02-17 15:50:20 -0300

    Instalação do dotenv

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import RouterAplication from './routes'
 import bodyParser from 'body-parser'
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express()
 app.use(express.json())
 app.use(cors())
